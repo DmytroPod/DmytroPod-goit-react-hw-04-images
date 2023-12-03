6664879e006cafb5a772df6553a4f98854f6d9b1
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-12-03 17:53:47 +0200

    fin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,107 +1,118 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Loader from './Loader/Loader';
 import getImages from '../API/API';
 
-export class App extends Component {
-  state = {
-    results: [],
-    query: '',
-    page: 1,
-    totalImages: 0,
+const App = () => {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalImages, setTotalImages] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-    loading: false,
-  };
-
-  getQuery = formSearch => {
+  const getQuery = formSearch => {
     if (formSearch.trim() === '') {
       alert('Please, enter request');
       return;
     }
-    this.setState({ query: formSearch, page: 1, results: [], totalImages: 0 });
-  };
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
-      this.updateGallery();
+    setQuery(formSearch);
+    setPage(1);
+    setResults([]);
+    setTotalImages(0);
+  };
+  useEffect(() => {
+    if (query) {
+      const updateGallery = async () => {
+        try {
+          setLoading(true);
+          const data = await getImages(query, page);
+
+          setResults(prevState => [...prevState, ...data.hits]);
+          setTotalImages(data.totalHits);
+        } catch {
+          alert('Something went wrong');
+        } finally {
+          setLoading(false);
+        }
+      };
+      updateGallery();
     }
-  }
-  updateGallery = async () => {
-    try {
-      this.setState({ loading: true });
-      const data = await getImages(this.state.query, this.state.page);
+  }, [page, query]);
 
-      this.setState(prevState => ({
-        results: [...prevState.results, ...data.hits],
-        totalImages: data.totalHits,
-      }));
-    } catch {
-      alert('Something went wrong');
-    } finally {
-      this.setState({ loading: false });
-    }
-  };
-  loadMore = async () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const loadMore = async () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    const { results, loading, totalImages } = this.state;
-    return (
-      <>
-        <Searchbar getQuery={this.getQuery} />
-        <ImageGallery array={results} />
-        {results.length < totalImages && <Button onClick={this.loadMore} />}
+  return (
+    <>
+      <Searchbar getQuery={getQuery} />
+      <ImageGallery array={results} />
+      {results.length < totalImages && <Button onClick={loadMore} />}
 
-        {loading && <Loader />}
-      </>
-    );
-  }
-}
-// const App = () => {
-//   const [results, setResults] = useState([]);
-//   const [query, setQuery] = useState('');
-//   const [page, setPage] = useState(1);
-//   const [totalImages, setTotalImages] = useState(0);
-//   const [loading, setLoading] = useState(false);
+      {loading && <Loader />}
+    </>
+  );
+};
+export default App;
+// import React, { Component } from 'react';
+// export class App extends Component {
+//   state = {
+//     results: [],
+//     query: '',
+//     page: 1,
+//     totalImages: 0,
 
-//   const getQuery = formSearch => {
+//     loading: false,
+//   };
+
+//   getQuery = formSearch => {
 //     if (formSearch.trim() === '') {
 //       alert('Please, enter request');
 //       return;
 //     }
 //     this.setState({ query: formSearch, page: 1, results: [], totalImages: 0 });
-//     setQuery(formSearch);
-//     setPage(1);
-//     setResults([]);
 //   };
-//   const updateGallery = async () => {
-//     try {
-//       setLoading(true);
-//       const data = await getImages(query, page);
 
-//       setResults(prevState => [...prevState.results, ...data.hits]);
-//       setTotalImages(data.totalHits);
+//   componentDidUpdate(_, prevState) {
+//     if (
+//       prevState.query !== this.state.query ||
+//       prevState.page !== this.state.page
+//     ) {
+//       this.updateGallery();
+//     }
+//   }
+//   updateGallery = async () => {
+//     try {
+//       this.setState({ loading: true });
+//       const data = await getImages(this.state.query, this.state.page);
+
+//       this.setState(prevState => ({
+//         results: [...prevState.results, ...data.hits],
+//         totalImages: data.totalHits,
+//       }));
 //     } catch {
 //       alert('Something went wrong');
 //     } finally {
-//       setLoading(false);
+//       this.setState({ loading: false });
 //     }
 //   };
+//   loadMore = async () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
 
-//   return (
-//     <>
-//       <Searchbar getQuery={getQuery} />
-//       <ImageGallery array={results} />
-//       {results.length < totalImages && <Button onClick={loadMore} />}
+//   render() {
+//     const { results, loading, totalImages } = this.state;
+//     return (
+//       <>
+//         <Searchbar getQuery={this.getQuery} />
+//         <ImageGallery array={results} />
+//         {results.length < totalImages && <Button onClick={this.loadMore} />}
 
-//       {loading && <Loader />}
-//     </>
-//   );
-// };
-export default App;
+//         {loading && <Loader />}
+//       </>
+//     );
+//   }
+// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,52 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import css from './Modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.closeEsc);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.closeEsc);
-  }
-
-  closeEsc = event => {
-    event.code === 'Escape' && this.props.onClose();
+const Modal = ({ largeImg, onClose }) => {
+  useEffect(() => {
+    const closeEsc = event => {
+      event.code === 'Escape' && this.props.onClose();
+    };
+    document.addEventListener('keydown', closeEsc);
+    return () => {
+      document.removeEventListener('keydown', closeEsc);
+    };
+  }, [onClose]);
+  const handleClick = event => {
+    event.target === event.currentTarget && onClose();
   };
-  handleClick = event => {
-    event.target === event.currentTarget && this.props.onClose();
-  };
-
-  render() {
-    const { largeImg } = this.props;
-    return (
-      <div className={css.overlay} onClick={this.handleClick}>
-        <div className={css.modal}>
-          <img src={largeImg} alt="" />
-        </div>
+  return (
+    <div className={css.overlay} onClick={handleClick}>
+      <div className={css.modal}>
+        <img src={largeImg} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.closeEsc);
+//   }
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.closeEsc);
+//   }
+
+//   closeEsc = event => {
+//     event.code === 'Escape' && this.props.onClose();
+//   };
+//   handleClick = event => {
+//     event.target === event.currentTarget && this.props.onClose();
+//   };
+
+//   render() {
+//     const { largeImg } = this.props;
+//     return (
+//       <div className={css.overlay} onClick={this.handleClick}>
+//         <div className={css.modal}>
+//           <img src={largeImg} alt="" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 export default Modal;
